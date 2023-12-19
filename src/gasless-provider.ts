9e8cde5d0b9cb09e207316fd2480862b77ee3320
excaliborr <124819095+excaliborr@users.noreply.github.com>
2023-12-19 11:07:07 -0500

    chore: add linter and husky (#2)
    * chore: add linter and husky
    
    * fix: husky pre-commit
    
    * fix: husky pre-commit
    
    * fix: husky pre-commit

--- src/gasless-provider.ts
+++ src/gasless-provider.ts
@@ -1,17 +1,16 @@
-import { ethers } from "ethers";
-import { ProviderWrapper } from "hardhat/plugins";
-import { EIP1193Provider, RequestArguments } from "hardhat/types";
-import init from "debug";
-import axios, { AxiosInstance } from "axios";
-import { SIGNATURE_PROXY_ABI, SIGNATURE_PROXY_FACTORY_ADDRESS, SIGNATURE_PROXY_CHILD_INIT_CODE } from "./constants";
+import { ethers } from 'ethers';
+import { ProviderWrapper } from 'hardhat/plugins';
+import { EIP1193Provider, RequestArguments } from 'hardhat/types';
+import init from 'debug';
+import axios, { AxiosInstance } from 'axios';
+import { SIGNATURE_PROXY_ABI, SIGNATURE_PROXY_FACTORY_ADDRESS, SIGNATURE_PROXY_CHILD_INIT_CODE } from './constants';
 
-const log = init("hardhat:plugin:gasless");
+const log = init('hardhat:plugin:gasless');
 
 // TODO: Using wallets with no ETH does not work
 // TODO: Test this with value transfers
 // TODO: Test this with tx type 0, 1
 export class GaslessProvider extends ProviderWrapper {
-
   private readonly _sponsorAPI: AxiosInstance;
 
   constructor(
@@ -21,11 +20,11 @@ export class GaslessProvider extends ProviderWrapper {
   ) {
     super(_wrappedProvider);
 
-    this._sponsorAPI = axios.create({baseURL: this.sponsorUrl});
+    this._sponsorAPI = axios.create({ baseURL: this.sponsorUrl });
   }
 
   public request(args: RequestArguments): Promise<unknown> {
-    if (args.method === "eth_sendRawTransaction" && args.params !== undefined) {
+    if (args.method === 'eth_sendRawTransaction' && args.params !== undefined) {
       const params = this._getParams(args);
       return this._sendGaslessTransaction(params[0]);
     }
@@ -58,7 +57,7 @@ export class GaslessProvider extends ProviderWrapper {
       value,
       chainId,
       nonce: nextNonce,
-    })
+    });
     const signature = await this.encodeMessage(signer, to as string, data, value, chainId, nextNonce);
 
     // call the API
@@ -69,47 +68,72 @@ export class GaslessProvider extends ProviderWrapper {
       value,
       ...signature,
     };
-    log('Sending request to sponsor', { sponsor: this.sponsorUrl, request })
+    log('Sending request to sponsor', { sponsor: this.sponsorUrl, request });
     const sponsorResponse = await this._sponsorAPI.post<string[]>('/tx-signature', [request]);
     const txHash = sponsorResponse.data[0];
     log('Received tx hash from sponsor', txHash);
-    
+
     // return the tx hash
     return txHash;
   }
 
   private async getChainId(): Promise<number> {
-    const rawChainId = await this._wrappedProvider.request({method: "eth_chainId", params: []}) as string;
+    const rawChainId = (await this._wrappedProvider.request({
+      method: 'eth_chainId',
+      params: [],
+    })) as string;
     return parseInt(rawChainId);
-  };
+  }
 
   private getProxyAddress(signer: string): string {
     return ethers.utils.getCreate2Address(
       SIGNATURE_PROXY_FACTORY_ADDRESS,
       ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(['address'], [signer])),
-      ethers.utils.solidityKeccak256(['bytes'], [SIGNATURE_PROXY_CHILD_INIT_CODE])
+      ethers.utils.solidityKeccak256(['bytes'], [SIGNATURE_PROXY_CHILD_INIT_CODE]),
     );
   }
 
   private async isContractDeployed(address: string): Promise<boolean> {
-    const code = await this._wrappedProvider.request({method: 'eth_getCode', params: [address, 'latest']});
+    const code = await this._wrappedProvider.request({
+      method: 'eth_getCode',
+      params: [address, 'latest'],
+    });
     return code !== '0x';
   }
 
   private async getProxyNextNonce(address: string): Promise<number> {
     const proxy = new ethers.Contract(address, SIGNATURE_PROXY_ABI);
     const populatedNonceFetch = await proxy.populateTransaction.nextNonce();
-    const rawNonce = await this._wrappedProvider.request({method: "eth_call", params: [populatedNonceFetch, 'latest']}) as string;
+    const rawNonce = (await this._wrappedProvider.request({
+      method: 'eth_call',
+      params: [populatedNonceFetch, 'latest'],
+    })) as string;
     return parseInt(rawNonce);
   }
 
-  private async encodeMessage(signer: ethers.Wallet, to: string, data: string, value: number, chainId: number, nonce: number): Promise<{ r: string, s: string, v: number }> {
-    const encodedMessage = ethers.utils.defaultAbiCoder.encode(['address', 'bytes', 'uint256', 'uint256', 'uint256'], [to, data, value, chainId, nonce]);
+  private async encodeMessage(
+    signer: ethers.Wallet,
+    to: string,
+    data: string,
+    value: number,
+    chainId: number,
+    nonce: number,
+  ): Promise<{ r: string; s: string; v: number }> {
+    const encodedMessage = ethers.utils.defaultAbiCoder.encode(
+      ['address', 'bytes', 'uint256', 'uint256', 'uint256'],
+      [to, data, value, chainId, nonce],
+    );
     log('Encoding message', {
-      to, data, value, chainId, nonce
+      to,
+      data,
+      value,
+      chainId,
+      nonce,
     });
     const digest = ethers.utils.keccak256(encodedMessage);
-    const rawSignature = await ethers.utils.joinSignature(signer._signingKey().signDigest(toEthSignedMessageHash(digest)));
+    const rawSignature = await ethers.utils.joinSignature(
+      signer._signingKey().signDigest(toEthSignedMessageHash(digest)),
+    );
     const { r, s, v } = ethers.utils.splitSignature(rawSignature);
     return { r, s, v };
   }
--- test/project.test.ts
+++ test/project.test.ts
@@ -1,14 +1,13 @@
-import { assert } from "chai";
-import path from "path";
+import { assert } from 'chai';
 
-import { useEnvironment } from "./helpers";
+import { useEnvironment } from './helpers';
 
-describe("Integration tests examples", function () {
-  describe("HardhatConfig extension", function () {
-    useEnvironment("hardhat-project");
+describe('Integration tests examples', function () {
+  describe('HardhatConfig extension', function () {
+    useEnvironment('hardhat-project');
 
-    it("Should add the sponsoredUrl to the config", function () {
-      assert.equal(this.hre.config.networks.localhost.sponsorUrl, "foo");
+    it('Should add the sponsoredUrl to the config', function () {
+      assert.equal(this.hre.config.networks.localhost.sponsorUrl, 'foo');
     });
   });
 });

e636bb5a3395a08c51a979ed461dab7534269801
excaliborr <124819095+excaliborr@users.noreply.github.com>
2024-01-10 13:20:45 -0500

    docs: update readme (#21)
    * docs: add license
    
    * docs: update readme
    
    * fix: typo
    
    * chore: add supported chain section

--- test/fixture-projects/hardhat-project/hardhat.config.ts
+++ test/fixture-projects/hardhat-project/hardhat.config.ts
@@ -8,7 +8,7 @@ const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   networks: {
     localhost: {
-      sponsoredTransaction: {
+      sponsoredTransactions: {
         bundlerUrl: 'http://localhost:3000',
         paymasterUrl: 'http://localhost:3001',
         paymasterType: 'pimlico',

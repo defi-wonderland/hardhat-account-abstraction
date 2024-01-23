5111bdcab60d9253ca16e60e6fbc3075f7966dae
excaliborr <124819095+excaliborr@users.noreply.github.com>
2024-01-23 11:45:07 -0500

    test: create paymaster client (#35)
    * test: create paymaster client
    
    * chore: more asserts

---
+++ test/paymaster.test.ts
@@ -0,0 +1,71 @@
+import { assert } from 'chai';
+import { createPaymasterClient } from '../src/paymaster';
+import { PaymasterType } from './types';
+import { createPimlicoBundlerClient } from 'permissionless/clients/pimlico';
+import { http } from 'viem';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import * as Pm from '../src/paymasters';
+
+describe('Paymaster Construction', function () {
+  const bundlerClient = createPimlicoBundlerClient({
+    transport: http('http://localhost:3001'),
+  });
+
+  const endpoint = 'http://localhost:3002';
+
+  it('Should create a  pimlico paymaster client', function () {
+    const paymasterClient = createPaymasterClient(
+      'pimlico' as PaymasterType,
+      endpoint,
+      bundlerClient,
+      undefined,
+    ) as Pm.PimlicoPaymaster;
+
+    assert.isTrue(paymasterClient instanceof Pm.PimlicoPaymaster);
+    assert.isTrue(paymasterClient.endpoint instanceof JsonRpcProvider);
+    assert.equal(paymasterClient.endpoint.connection.url, endpoint);
+    assert.equal(paymasterClient.policyId, undefined);
+  });
+
+  it('Should create a  stackup paymaster client', function () {
+    const paymasterClient = createPaymasterClient('stackup' as PaymasterType, endpoint, bundlerClient, undefined);
+
+    assert.isTrue(paymasterClient instanceof Pm.StackUpPaymaster);
+    assert.isTrue(paymasterClient.endpoint instanceof JsonRpcProvider);
+    assert.equal(paymasterClient.endpoint.connection.url, endpoint);
+  });
+
+  it('Should create a  base paymaster client', function () {
+    const paymasterClient = createPaymasterClient('base' as PaymasterType, endpoint, bundlerClient, undefined);
+
+    assert.isTrue(paymasterClient instanceof Pm.BasePaymaster);
+    assert.isTrue(paymasterClient.endpoint instanceof JsonRpcProvider);
+    assert.equal(paymasterClient.endpoint.connection.url, endpoint);
+  });
+
+  it('Should create a  alchemy paymaster client', function () {
+    const paymasterClient = createPaymasterClient(
+      'alchemy' as PaymasterType,
+      endpoint,
+      bundlerClient,
+      'test',
+    ) as Pm.AlchemyPaymaster;
+
+    assert.isTrue(paymasterClient instanceof Pm.AlchemyPaymaster);
+    assert.isTrue(paymasterClient.endpoint instanceof JsonRpcProvider);
+    assert.equal(paymasterClient.endpoint.connection.url, endpoint);
+    assert.equal(paymasterClient.policyId, 'test');
+  });
+
+  it('Should fail to  create a  alchemy paymaster client if there is no policy id', function () {
+    assert.throws(() => {
+      createPaymasterClient('alchemy' as PaymasterType, endpoint, bundlerClient, undefined);
+    }, 'Policy ID is required for Alchemy Paymaster');
+  });
+
+  it('Should fail to  create a  unknown paymaster client', function () {
+    assert.throws(() => {
+      createPaymasterClient('unknown' as PaymasterType, endpoint, bundlerClient, undefined);
+    }, 'Unknown paymaster type unknown');
+  });
+});

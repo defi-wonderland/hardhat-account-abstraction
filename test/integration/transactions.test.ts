5935da3622a5c2f6245f2fb1de034790415aaf17
Austrian <114922365+0xAustrian@users.noreply.github.com>
2024-02-01 09:53:25 -0300

    test: move duplicated code to before (#53)

--- test/integration/transactions.test.ts
+++ test/integration/transactions.test.ts
@@ -1,19 +1,29 @@
 import { assert } from 'chai';
 import { useEnvironment } from '../helpers';
 import { TEST_TOKEN_ABI, tokenAddr, entryPoint } from '../test-constants';
+import { Contract } from 'ethers';
+import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
 import 'dotenv/config';
 
 describe('Integration transactions', function () {
   useEnvironment('integration');
 
+  let signer: HardhatEthersSigner;
+  let signerAddress: string;
+  let testToken: Contract;
+
+  before(async function () {
+    signer = await this.hre.ethers.provider.getSigner();
+    signerAddress = await signer.getAddress();
+    testToken = new this.hre.ethers.Contract(tokenAddr, TEST_TOKEN_ABI, signer);
+  });
+
   it('Should send a transaction', async function () {
-    const signer = await this.hre.ethers.provider.getSigner();
     const smartAccount = await this.hre.network.provider.request({
       method: 'sponsored_getSmartAccountAddress',
-      params: [await signer.getAddress()],
+      params: [signerAddress],
     });
 
-    const testToken = new this.hre.ethers.Contract(tokenAddr, TEST_TOKEN_ABI, signer);
     const amountToMint = this.hre.ethers.parseEther('6.9');
     const receipt = await testToken.mint(amountToMint);
     const balanceOf = await testToken.balanceOf(smartAccount);
@@ -23,14 +33,10 @@ describe('Integration transactions', function () {
   });
 
   it('Should transfer tokens from the smart account', async function () {
-    const signer = await this.hre.ethers.provider.getSigner();
-    const signerAddr = await signer.getAddress();
-
-    const testToken = new this.hre.ethers.Contract(tokenAddr, TEST_TOKEN_ABI, signer);
     const amountToTransfer = this.hre.ethers.parseEther('1.0');
 
-    const receipt = await testToken.transfer(signerAddr, amountToTransfer);
-    const balanceOf = await testToken.balanceOf(signerAddr);
+    const receipt = await testToken.transfer(signerAddress, amountToTransfer);
+    const balanceOf = await testToken.balanceOf(signerAddress);
 
     assert.equal(receipt.to, entryPoint);
     assert.equal(balanceOf.toString(), amountToTransfer.toString());
